bee00e7703a072d128926779313f146448c0b020
CuiweiG <aid@agentmail.to>
2026-03-28 02:40:15 +0800

    INIT: package skeleton with DESCRIPTION, LICENSE, testthat

---
+++ R/survinger-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom rlang .data .env := %||%
+#' @importFrom stats optim qnorm pnorm dnbinom pnbinom rpois rbinom
+#'   rnbinom var median quantile sd weighted.mean dpois ppois plnorm
+#'   dlnorm na.omit setNames
+#' @importFrom methods is
+#' @importFrom utils head tail
+NULL
